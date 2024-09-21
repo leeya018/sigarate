371460cec5aec4436da3a031af242fa892dcdcfa
leeya018 <leeyahav018@gmail.com>
2024-09-21 19:20:01 +0300

    fix button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,48 +3,48 @@ import React, { useState, useEffect } from "react";
 import Header from "@/components/Header";
 
 const Main = () => {
-  const [cigarettesLeft, setCigarettesLeft] = useState(0);
+  const [cigarettesLeft, setCigarettesLeft] = useState<number>(0);
 
   const checkReset = () => {
-    const initialCigarettes =
-      parseInt(localStorage.getItem("initialCigarettes"), 10) || 0;
+    const initialCigarettes = parseInt(
+      localStorage.getItem("initialCigarettes") || "20",
+      10
+    ); // Handle null case by providing "0"
     const lastResetTime = localStorage.getItem("lastResetTime");
     const now = new Date();
 
     if (lastResetTime) {
       const lastResetDate = new Date(lastResetTime);
       const timePassed = now.getTime() - lastResetDate.getTime();
-      // const oneDayInMs = 5 * 1000; // milliseconds in one day
+      // const oneDayInMs = 4 * 1000; // milliseconds in one day
       const oneDayInMs = 24 * 60 * 60 * 1000; // milliseconds in one day
 
       if (timePassed >= oneDayInMs) {
         // More than a day has passed, reset the cigarette count
-        setCigarettesLeft(initialCigarettes);
-
         const decreaseAmount = parseInt(
-          localStorage.getItem("decreaseAmount"),
+          localStorage.getItem("decreaseAmount") || "0", // Handle null case by providing "0"
           10
         );
-        localStorage.setItem(
-          "cigarettesLeft",
-          initialCigarettes - decreaseAmount
-        ) || 0;
-        localStorage.setItem(
-          "initialCigarettes",
-          initialCigarettes - decreaseAmount
-        ) || 0;
+        const newCigarettesLeft =
+          initialCigarettes - decreaseAmount > 0
+            ? initialCigarettes - decreaseAmount
+            : 0;
+        setCigarettesLeft(newCigarettesLeft);
+
+        localStorage.setItem("cigarettesLeft", newCigarettesLeft.toString());
+        localStorage.setItem("initialCigarettes", newCigarettesLeft.toString());
         localStorage.setItem("lastResetTime", now.toISOString());
       } else {
         // Less than a day has passed, retrieve the remaining cigarettes from localStorage
         const storedCigarettes =
-          parseInt(localStorage.getItem("cigarettesLeft"), 10) ||
+          parseInt(localStorage.getItem("cigarettesLeft") || "0", 10) ||
           initialCigarettes;
         setCigarettesLeft(storedCigarettes);
       }
     } else {
       // First time or reset hasn't been done yet, set the initial values
       setCigarettesLeft(initialCigarettes);
-      localStorage.setItem("cigarettesLeft", initialCigarettes);
+      localStorage.setItem("cigarettesLeft", initialCigarettes.toString());
       localStorage.setItem("lastResetTime", now.toISOString());
     }
   };
@@ -59,7 +59,7 @@ const Main = () => {
     if (cigarettesLeft > 0) {
       const newCigarettesLeft = cigarettesLeft - 1;
       setCigarettesLeft(newCigarettesLeft);
-      localStorage.setItem("cigarettesLeft", newCigarettesLeft);
+      localStorage.setItem("cigarettesLeft", newCigarettesLeft.toString()); // Ensure value is stored as string
     }
   };
 
@@ -73,12 +73,14 @@ const Main = () => {
         <div className="text-6xl font-bold text-blue-600 mb-6">
           {cigarettesLeft}
         </div>
-        <button
-          onClick={handleDecrement}
-          className="bg-blue-600 text-white px-6 py-3 rounded-lg font-semibold hover:bg-blue-700"
-        >
-          Smoke 1
-        </button>
+        {cigarettesLeft > 0 && (
+          <button
+            onClick={handleDecrement}
+            className="bg-blue-600 text-white px-6 py-3 rounded-lg font-semibold hover:bg-blue-700"
+          >
+            Smoke 1
+          </button>
+        )}
       </main>
     </div>
   );
--- src/app/settings/page.tsx
+++ src/app/settings/page.tsx
@@ -4,20 +4,25 @@ import Header from "@/components/Header";
 import React, { useState } from "react";
 
 const Settings = () => {
-  const [initialCigarettes, setInitialCigarettes] = useState(
-    parseInt(localStorage.getItem("initialCigarettes"), 10) || 12
+  const [hasChange, sethasChange] = useState(true);
+  const [initialCigarettes, setInitialCigarettes] = useState<number>(
+    parseInt(localStorage.getItem("initialCigarettes") || "12", 10) || 20 // Fallback to "12" if null
   );
-  const [decreaseAmount, setDecreaseAmount] = useState(
-    parseInt(localStorage.getItem("decreaseAmount"), 10) || 1
+  const [decreaseAmount, setDecreaseAmount] = useState<number>(
+    parseInt(localStorage.getItem("decreaseAmount") || "1", 10) // Fallback to "1" if null
   );
-  const [daysBetweenDecreases, setDaysBetweenDecreases] = useState(
-    parseInt(localStorage.getItem("daysBetweenDecreases"), 10) || 1
+  const [daysBetweenDecreases, setDaysBetweenDecreases] = useState<number>(
+    parseInt(localStorage.getItem("daysBetweenDecreases") || "1", 10) // Fallback to "1" if null
   );
 
   const handleSave = () => {
-    localStorage.setItem("initialCigarettes", initialCigarettes);
-    localStorage.setItem("decreaseAmount", decreaseAmount);
-    localStorage.setItem("daysBetweenDecreases", daysBetweenDecreases);
+    sethasChange(false);
+    localStorage.setItem("initialCigarettes", initialCigarettes.toString()); // Ensure the value is stored as a string
+    localStorage.setItem("decreaseAmount", decreaseAmount.toString()); // Ensure the value is stored as a string
+    localStorage.setItem(
+      "daysBetweenDecreases",
+      daysBetweenDecreases.toString()
+    ); // Ensure the value is stored as a string
   };
 
   return (
@@ -25,14 +30,17 @@ const Settings = () => {
       <Header />
       <main className="flex-grow flex flex-col justify-center items-center text-center px-4">
         <h1 className="text-3xl font-bold mb-6">Settings</h1>
-        <div className="mb-4  max-w-xs">
+        <div className="mb-4 max-w-xs">
           <label className="block text-left font-semibold mb-2">
             Cigarettes per Day:
           </label>
           <input
             type="number"
             value={initialCigarettes}
-            onChange={(e) => setInitialCigarettes(parseInt(e.target.value, 10))}
+            onChange={(e) => {
+              setInitialCigarettes(parseInt(e.target.value, 10));
+              sethasChange(true);
+            }}
             className="w-full px-3 py-2 border rounded-lg"
           />
         </div>
@@ -43,26 +51,32 @@ const Settings = () => {
           <input
             type="number"
             value={decreaseAmount}
-            onChange={(e) => setDecreaseAmount(parseInt(e.target.value, 10))}
+            onChange={(e) => {
+              setDecreaseAmount(parseInt(e.target.value, 10));
+              sethasChange(true);
+            }}
             className="w-full px-3 py-2 border rounded-lg"
           />
         </div>
-        <div className="mb-6  max-w-xs">
+        <div className="mb-6 max-w-xs">
           <label className="block text-left font-semibold mb-2">
             Days between Decreases:
           </label>
           <input
             type="number"
             value={daysBetweenDecreases}
-            onChange={(e) =>
-              setDaysBetweenDecreases(parseInt(e.target.value, 10))
-            }
+            onChange={(e) => {
+              setDaysBetweenDecreases(parseInt(e.target.value, 10));
+              sethasChange(true);
+            }}
             className="w-full px-3 py-2 border rounded-lg"
           />
         </div>
         <button
           onClick={handleSave}
-          className="bg-blue-600 text-white px-6 py-3 rounded-lg font-semibold hover:bg-blue-700"
+          className={`${
+            hasChange ? "bg-blue-600 hover:bg-blue-700" : "bg-gray-600 "
+          }  text-white px-6 py-3 rounded-lg font-semibold`}
         >
           Save
         </button>
